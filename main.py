5bd4762ceb51108c29397366aec0c34aa4fcf69b
Thamsanqa Hadebe <thamsanqahadebe98@gmail.com>
2026-06-18 21:16:19 +0200

    update: main logic applied

---
+++ main.py
@@ -0,0 +1,20 @@
+from extract import fetch_weather
+from transform import transform_weather
+from load import load_to_database
+
+def run_pipeline():
+
+    print("Extracting data...")
+    data = fetch_weather()
+
+    print("Transforming data...")
+    df = transform_weather(data)
+
+    print("Loading data to database...")
+    load_to_database(df)
+
+    print("Pipeline Completed!")
+    
+
+if __name__ == "__main__":
+    run_pipeline()
